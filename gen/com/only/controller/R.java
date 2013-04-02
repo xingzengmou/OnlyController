a8ee95f4238d6fe848b394c0f5fb58b4b02d8fe4
xingzengmou <xingzengmou@gmail.com>
2013-04-02 19:53:52 +0800

    add files

--- gen/com/only/controller/R.java
+++ gen/com/only/controller/R.java
@@ -10,6 +10,10 @@ package com.only.controller;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int main_body_color=0x7f040000;
+        public static final int white_color=0x7f040001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,36 +21,44 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int focus=0x7f020000;
-        public static final int game_setting=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int key_setting=0x7f020003;
-        public static final int setting=0x7f020004;
-        public static final int title=0x7f020005;
-        public static final int touch_setting=0x7f020006;
+        public static final int btn_d=0x7f020000;
+        public static final int btn_n=0x7f020001;
+        public static final int focus=0x7f020002;
+        public static final int game_list_btn=0x7f020003;
+        public static final int game_setting=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int key_setting=0x7f020006;
+        public static final int line=0x7f020007;
+        public static final int setting=0x7f020008;
+        public static final int title=0x7f020009;
+        public static final int touch_setting=0x7f02000a;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
-        public static final int content_view_ly=0x7f080000;
-        public static final int game_setting_btn=0x7f080001;
-        public static final int key_setting_btn=0x7f080002;
-        public static final int setting_btn=0x7f080004;
-        public static final int touch_setting_btn=0x7f080003;
+        public static final int action_settings=0x7f090005;
+        public static final int content_ly=0x7f090000;
+        public static final int game_list_btn=0x7f090001;
+        public static final int icon_iv=0x7f090004;
+        public static final int key_config_btn=0x7f090002;
+        public static final int touch_config_btn=0x7f090003;
     }
     public static final class layout {
         public static final int activity_only_main=0x7f030000;
+        public static final int view_game_config_files=0x7f030001;
     }
     public static final class menu {
-        public static final int only_main=0x7f070000;
+        public static final int only_main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int game_list_btn=0x7f060003;
+        public static final int hello_world=0x7f060002;
+        public static final int key_config_btn=0x7f060004;
+        public static final int touch_config_btn=0x7f060005;
     }
     public static final class style {
         /** 
@@ -70,10 +82,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- src/com/only/controller/OnlyMainActivity.java
+++ src/com/only/controller/OnlyMainActivity.java
@@ -3,20 +3,68 @@ package com.only.controller;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class OnlyMainActivity extends Activity {
+	private static final String TAG = "OnlyMainActivity";
+	
+	/**
+	 * Buttons define
+	 */
+	private Button btnGameConfigFiles;
+	private Button btnKeyConfig;
+	private Button btnTouchConfig;
+	/**
+	 * LinearLayout define
+	 */
+	private LinearLayout lyContent;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_only_main);
+		getViewHandles();
+		setViewListener();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.only_main, menu);
-		return true;
+	private void getViewHandles() {
+		btnGameConfigFiles = (Button) findViewById(R.id.game_list_btn);
+		btnKeyConfig = (Button) findViewById(R.id.key_config_btn);
+		btnTouchConfig = (Button) findViewById(R.id.touch_config_btn);
+		lyContent = (LinearLayout) findViewById(R.id.content_ly);
 	}
-
+	
+	private void setViewListener() {
+		btnGameConfigFiles.setOnClickListener(btnOnClicks);
+		btnKeyConfig.setOnClickListener(btnOnClicks);
+		btnTouchConfig.setOnClickListener(btnOnClicks);
+	}
+	
+	
+	private View.OnClickListener btnOnClicks = new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			if (v.equals(btnGameConfigFiles)) {
+				switchBackground(btnGameConfigFiles, true);
+				switchBackground(btnKeyConfig, false);
+				switchBackground(btnTouchConfig, false);
+			} else if (v.equals(btnKeyConfig)) {
+				switchBackground(btnGameConfigFiles, false);
+				switchBackground(btnKeyConfig, true);
+				switchBackground(btnTouchConfig, false);
+			} else if (v.equals(btnTouchConfig)) {
+				switchBackground(btnGameConfigFiles, false);
+				switchBackground(btnKeyConfig, false);
+				switchBackground(btnTouchConfig, true);
+			}
+		}
+	};
+	
+	private void switchBackground(View v, boolean d) {
+		v.setBackgroundResource(d ? R.drawable.btn_d : R.drawable.btn_n);
+	}
 }
